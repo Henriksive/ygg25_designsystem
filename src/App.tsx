b054671b8aa96016700d7f1cf8e21c6bc9c9e5b7
Henrik Sivertsgård <henriksive@gmail.com>
2025-03-31 15:12:50 +0200

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 // Kommenter ut linjen ved å fjerne // og bytt ut DITT_TEMA med navnet på temaet ditt
-// import '../design-tokens-build/DITT_TEMA.css'
-import { Button, Heading, Paragraph } from '@digdir/designsystemet-react';
-import './App.css'
+import "../design-tokens-build/theme.css";
+import { Button, Heading, Paragraph } from "@digdir/designsystemet-react";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Heading data-size='lg' level={1}>Velkommen til Designsystemet workshop.</Heading>
-      <Paragraph>Redigere kildekoden i <code>App.tsx</code> for å endre utseendet på denne siden</Paragraph>
-      <Button >Knapp</Button>
-    </>
-  )
+    <div data-color="accent" data-color-scheme="light">
+      <Heading data-size="lg" level={1}>
+        Velkommen til Designsystemet workshop.
+      </Heading>
+      <div className="minklasse">
+        <Paragraph>
+          Redigere kildekoden i <code>App.tsx</code> for å endre utseendet på
+          denne siden
+        </Paragraph>
+      </div>
+      <Button>Knapp</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
